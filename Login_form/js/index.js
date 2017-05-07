d284337cc3602a3c49c1367940211109fa946013
Obsaa Abdalhalim <obsaaabdalhalim@Obsaas-MacBook-Pro.local>
2017-05-07 07:29:39 -0700

    A lot of changes

--- Login_form/js/index.js
+++ Login_form/js/index.js
@@ -1,3 +1,28 @@
 $('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+	$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+$(".register-form").validate({
+	rules: 
+	{
+		password:
+		{
+			required: true,
+			minlength: 6,
+			maxlength: 15
+		},
+		cfmPassword: 
+		{
+			equalTo: "#password",
+			minlength: 6,
+			maxlength: 15
+		}
+	},
+	messages:
+	{
+		password:
+		{
+			required:"the password is required"
+		}
+	}
 });
